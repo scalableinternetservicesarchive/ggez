8dca8a8d2037d32301403b57da591a6c3dc199c4
Devenvimalpatel <devenvimalpatel@gmail.com>
2019-10-18 01:24:21 +0000

    Template tutorial page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,12 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree .
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
