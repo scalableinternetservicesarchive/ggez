e640ab975b98cd67218bb8671e1898e62f91d99e
Christopher Aziz <caziz@ucla.edu>
2019-10-22 22:31:15 -0700

    Address nit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -46,6 +46,6 @@ class ReviewsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def review_params
-    params.require(:review).permit(:rating, :content, :user_id, :resume_id)
+    params.require(:review).permit(:rating, :content, :resume_id)
   end
 end
