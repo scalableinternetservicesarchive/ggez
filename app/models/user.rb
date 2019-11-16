5ee8bc367805aea7f85a94c3d72cf1359779ed71
Aidan Wolk <aidanwolk@comcast.net>
2019-11-16 01:00:15 -0800

    fix user -> review cascading deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :industry, presence: true
   has_many :resumes, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 
   has_secure_password
 end
