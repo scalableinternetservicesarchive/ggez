824addb10e922c28020b112f1ce620bae2087313
Christopher Aziz <caziz@ucla.edu>
2019-11-28 14:51:08 -0800

    Add seed/load-test for lots of comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,55 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-example_user = User.create!(
-    username: 'example', industry: 'CS',
-    password: 'a', password_confirmation: 'a',
-    points: 1_000_000_000
+# Create CS user for general load testing
+
+example_cs_user = User.create!(
+  username: 'example',
+  industry: 'CS',
+  password: 'a',
+  password_confirmation: 'a',
+  points: 1_000_000_000
 )
 
-example_resume = Resume.create!(
-    user_id: example_user.id,
-    public: true,
-    file: Rails.root.join("app/assets/images/dummy.pdf").open
+Resume.create!(
+  user: example_cs_user,
+  public: true,
+  file: Rails.root.join('app/assets/images/dummy.pdf').open
 )
+
+# Create a Finance user which will have lots of reviews
+
+example_finance_user = User.create!(
+  username: 'example',
+  industry: 'finance',
+  password: 'a',
+  password_confirmation: 'a',
+  points: 1_000_000_000
+)
+
+example_finance_resume = Resume.create!(
+  user: example_finance_user,
+  public: true,
+  file: Rails.root.join('app/assets/images/dummy.pdf').open
+)
+
+# Create lots more users and reviews
+100.times do |n|
+  new_user = User.create!(
+    username: "sample_user_#{n}",
+    industry: 'finance',
+    password: 'a',
+    password_confirmation: 'a',
+    points: 5
+  )
+  comments = ['I like this resume. Good work!',
+              'This resume is perfect!!!',
+              'Bad resume.',
+              'Needs some work']
+  Review.create!(
+    rating: rand(0..5),
+    content: comments.sample,
+    user: new_user,
+    resume: example_finance_resume
+  )
+end
